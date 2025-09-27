427fad72cb2d8e1501e44c039dfb26b3dbb1eba1
Sapi <67680687+sapiBeCookin@users.noreply.github.com>
2025-09-27 17:03:08 +0200

    Aggiunti Dto

---
+++ Dto/TaskDto.cs
@@ -0,0 +1,9 @@
+namespace TaskList.Dto
+{
+    public class TaskNoId
+    {
+        public int IdUser { get; set; }
+        public string? Task { get; set; }
+        public bool TaskStatus { get; set; }
+    }
+}
---
+++ Dto/UserDto.cs
@@ -0,0 +1,8 @@
+namespace TaskList.Dto
+{
+    public class UserNoId
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
