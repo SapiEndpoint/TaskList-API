57480e59898ef93cadb3163fbdf01233c9b79bcc
Sapi <67680687+sapiBeCookin@users.noreply.github.com>
2025-10-05 03:00:48 +0200

    Validazione dei dati con Data Annotations e gestione errori personalizzati (DTO Compresi)
    Implementate Data Annotations nei modelli e nei DTO per la validazione automatica dei dati in ingresso.
    Aggiunte regole di validazione personalizzate e messaggi di errore custom per migliorare la chiarezza in caso di input non valido.

--- Models/TaskManagement.cs
+++ Models/TaskManagement.cs
@@ -1,12 +1,26 @@
-﻿namespace TaskList.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskList.Models
 {
     public class TaskManagement
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdTask { get; set; }
+
+        [Required(ErrorMessage = "L'id utente e' obbligatorio.")]
         public int IdUser { get; set; }
+
+        [Required]
+        [MaxLength(255, ErrorMessage = "La task non puo' superare i 255 caratteri.")]
+        [RegularExpression(@"^[A-Z][a-z0-9]*(\s[A-Za-z0-9]+)*$", ErrorMessage = "La task deve contenere solo lettere (la prima maiuscola) e numeri.")]
         public string? Task { get; set; }
+
+        [Required(ErrorMessage = "Lo stato e' obbligatorio.")]
         public bool TaskStatus { get; set; }
 
-        public Users? User { get; set; }  
+        [ForeignKey("IdUser")]
+        public Users? User { get; set; }
     }
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -1,9 +1,22 @@
-﻿namespace TaskList.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskList.Models
 {
     public class Users
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdUser { get; set; }
+
+        [Required]
+        [MaxLength(255, ErrorMessage = "Il nome non puo' superare i 255 caratteri.")]
+        [RegularExpression(@"^[A-Z][a-z]+", ErrorMessage = "Il nome deve contenere solo lettere (la prima maiuscola).")]
         public string? FirstName { get; set; }
+
+        [Required]
+        [MaxLength(255, ErrorMessage = "Il cognome non puo' superare i 255 caratteri.")]
+        [RegularExpression(@"^[A-Z][a-z]+", ErrorMessage = "Il cognome deve contenere solo lettere (la prima maiuscola).")]
         public string? LastName { get; set; }
 
         public ICollection<TaskManagement>? Task { get; set; }
